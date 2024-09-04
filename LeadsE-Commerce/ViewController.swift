e653b1347a0b9041b4f5eab88ed319361669264f
mamunabdullah <mamundigitalride@gmail.com>
2024-09-04 08:31:14 +0600

    Initial Commit

---
+++ LeadsE-Commerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeadsE-Commerce
+//
+//  Created by Abdullah Al Mamun on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LeadsE-CommerceUITests/LeadsE_CommerceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LeadsE_CommerceUITestsLaunchTests.swift
+//  LeadsE-CommerceUITests
+//
+//  Created by Abdullah Al Mamun on 4/9/24.
+//
+
+import XCTest
+
+final class LeadsE_CommerceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
